ee75938bf2b7c2bba6b21ea4ce490b26b90b75cd
chauncygu <gshangd@163.com>
2022-06-07 10:10:11 +0200

    fix hand_base

--- dexteroushandenvs/tasks/shadow_hand_two_catch_underarm.py
+++ dexteroushandenvs/tasks/shadow_hand_two_catch_underarm.py
@@ -12,7 +12,7 @@ import random
 import torch
 
 from utils.torch_jit_utils import *
-from tasks.hand_base.base_task import BaseTask
+from tasks.base.base_task import BaseTask
 from isaacgym import gymtorch
 from isaacgym import gymapi
 
